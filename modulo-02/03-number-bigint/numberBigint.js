91e73854a7b4d71ecdbe7742407f1ca836efedf4
allansmithll <allanammancio2004@gmail.com>
2023-08-27 15:56:25 -0300

    Aula de number e bigint

---
+++ modulo-02/03-number-bigint/numberBigint.js
@@ -0,0 +1,20 @@
+"use strict";
+// => Exemplos: number
+let num1 = 23;
+let num2 = 0x78CF;
+let num3 = 0o577;
+let num4 = 0b110001;
+console.log('Number - Ponto Flutuante ... : ', num1);
+console.log('Number - Hexadecimal  ... : ', num2);
+console.log('Number - Octal ... : ', num3);
+console.log('Number - Binário ... : ', num4);
+
+// => Exemplos: bigint
+let big1 = 9007199254740991n;
+let big2 = 1099511627779n;
+let big3 = 0x2000000000003n;
+let big4 = 9007199254740995n;
+console.log('Bigint ... : ', big1);
+console.log('Bigint - Binário ... : ', big2);
+console.log('Bigint - Hexadecimal ... : ', big3);
+console.log('Bigint - Octal ... : ', big4);
